5e38988aff34c11fa17f72334cdcae3b931e3890
utsav-04 <100127451+utsav-04@users.noreply.github.com>
2022-03-07 00:16:45 +0530

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,128 @@
+let allMusic = [
+    {
+        name: "Bachalo",
+        artist: "Akhil",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Chal Ghar Chalen",
+        artist: "Arijit Singh",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Dil Jaaniye",
+        artist: "Jubin Nautiyal",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Excuses",
+        artist: "A P Dhillon",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "Khaab",
+        artist: "Akhil",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "Phulkari",
+        artist: "Karan Randhawa",
+        img: "music-6",
+        src: "music-6"
+    },
+    {
+        name: "Manjha",
+        artist: "Vishal Mishra",
+        img: "music-7",
+        src: "music-7"
+    },
+    {
+        name: "Moonlight",
+        artist: "Harnoor",
+        img: "music-8",
+        src: "music-8"
+    },
+    {
+        name: "Do-Gallan",
+        artist: "Neha Kakkar",
+        img: "music-9",
+        src: "music-9"
+    },
+    {
+        name: "Ikko mikke",
+        artist: "Satinder Sartaaj",
+        img: "music-10",
+        src: "music-10"
+    },
+    {
+        name: "Prada",
+        artist: "Jass Manak",
+        img: "music-11",
+        src: "music-11"
+    },
+    {
+        name: "Tujhe kaise pata na chala",
+        artist: "Asees Kaur",
+        img: "music-12",
+        src: "music-12"
+    },
+    {
+        name: "lehanga",
+        artist: "Jass Manak",
+        img: "music-13",
+        src: "music-13"
+    },
+    {
+        name: "Pyaar Karte ho na",
+        artist: "Stebin bin",
+        img: "music-14",
+        src: "music-14"
+    },
+    {
+        name: "Aashiqui Aa gyi",
+        artist: "Arijit Singh",
+        img: "music-15",
+        src: "music-15"
+    },
+    {
+        name: "Raataan Lambiyan",
+        artist: "Jubin Nautiyal",
+        img: "music-16",
+        src: "music-16"
+    },
+    {
+        name: "Ranjha",
+        artist: "B Praak",
+        img: "music-17",
+        src: "music-17"
+    },
+    {
+        name: "Kabhii Tumhe",
+        artist: "Darshan Raval",
+        img: "music-18",
+        src: "music-18"
+    },
+    {
+        name: "Maan Bharryaa",
+        artist: "B Praak",
+        img: "music-19",
+        src: "music-19"
+    },
+    {
+        name: "Teri Kami",
+        artist: "Akhil",
+        img: "music-20",
+        src: "music-20"
+    },
+    {
+        name: "Saada Pyaar",
+        artist: "A P Dhillon",
+        img: "music-21",
+        src: "music-21"
+    },
+];
